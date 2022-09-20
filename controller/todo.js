5afe89340286cd1f9450049f6cdfcc4a66dc3ad4
changhyeonlee-0703 <changhyeonlee9473@gmail.com>
2022-09-20 20:04:58 +0900

    todo mvc구조 마지막 수정

--- controller/todo.js
+++ controller/todo.js
@@ -1,57 +1,114 @@
+// user가 있는 버전(server에 들어가는 버전). 이 repo에선 user가 없는 버전이 동작됨.
 const todo = require("../models/todo");
 
+exports.createTodo = asyncWrapper(async (req, res) => {
+  const user = req.locals;
+  const { work, isDone, color } = req.body;
+  if (!work || !color || typeof isDone !== "boolean") {
+    throw new Error("입력 값이 없습니다.");
+  }
+  const result = await todo.createTodo(work, isDone, color, user);
+  res.status(200).json(result);
+});
 
-exports.createTodo = async (req, res) => {
-    try{
-        const {work, isDone, color} = req.body;
-        const result = await todo.createTodo(work, isDone, color);
-        res.status(200).json(result);
+exports.getTodo = asyncWrapper(async (req, res) => {
+  const user = req.locals;
+  const day = req.params.day;
+  const result = await todo.getTodo(day, user); //userId
+  res.status(200).json({ todoArr: result.todoArr });
+});
+
+exports.putTodo = asyncWrapper(async (req, res) => {
+  const user = req.locals;
+  if (req.body.isDone === true || req.body.isDone === false) {
+    const todoId = req.params.id;
+    const { isDone } = req.body;
+    if (typeof isDone !== "boolean") {
+      throw new Error("isDone이 없습니다.");
     }
-    catch(err){
-        res.status(400).json({errMessage: err});
+    const result = await todo.isDoneTodo(todoId, isDone, user);
+    res.status(200).json(result);
+  } else {
+    const todoId = req.params.id;
+    const { work, color } = req.body;
+    if (!work || !color) {
+      throw new Error("입력 값이 없습니다.");
     }
+    const result = await todo.putTodo(todoId, work, color, user);
+    res.status(200).json(result);
+  }
+});
+
+exports.deleteTodo = asyncWrapper(async (req, res) => {
+  try {
+    const user = req.locals;
+    const todoId = req.params.id;
+    if (!todoId) {
+      throw new Error("todo Id가 없습니다.");
+    }
+    const result = await todo.deleteTodo(todoId, user); //userId
+    res.status(200).json({ success: result });
+  } catch (err) {
+    res.status(400).json({ errMessage: err });
+  }
+});
+
+
+// user가 없는 버전
+// const todo = require("../models/todo");
+
+
+// exports.createTodo = async (req, res) => {
+//     try{
+//         const {work, isDone, color} = req.body;
+//         const result = await todo.createTodo(work, isDone, color);
+//         res.status(200).json(result);
+//     }
+//     catch(err){
+//         res.status(400).json({errMessage: err});
+//     }
     
-};
+// };
 
-exports.getTodo = async (req, res) => {
-    try{
-        const day = req.params.day;
-        const result = await todo.getTodo(day); //userId
-        res.status(200).json({ todoArr : result.todoArr});
-    }
-    catch(err){
-        res.status(400).json({errMessage: err});
-    }
-};
+// exports.getTodo = async (req, res) => {
+//     try{
+//         const day = req.params.day;
+//         const result = await todo.getTodo(day); //userId
+//         res.status(200).json({ todoArr : result.todoArr});
+//     }
+//     catch(err){
+//         res.status(400).json({errMessage: err});
+//     }
+// };
 
-exports.putTodo = async (req, res) => {
-    try{
-        if (req.body.isDone === true || req.body.isDone === false){
-            const todoId = req.params.id;
-            const {isDone} = req.body;
-            const result = await todo.isDoneTodo(todoId, isDone);
-            res.status(200).json(result);
+// exports.putTodo = async (req, res) => {
+//     try{
+//         if (req.body.isDone === true || req.body.isDone === false){
+//             const todoId = req.params.id;
+//             const {isDone} = req.body;
+//             const result = await todo.isDoneTodo(todoId, isDone);
+//             res.status(200).json(result);
             
-        }
-        else{
-            const todoId = req.params.id;
-            const {work, color} = req.body;
-            const result = await todo.putTodo(todoId, work, color);
-            res.status(200).json(result);
-        }
-    }
-    catch(err){
-        res.status(400).json({errMessage: err});
-    }
-};
+//         }
+//         else{
+//             const todoId = req.params.id;
+//             const {work, color} = req.body;
+//             const result = await todo.putTodo(todoId, work, color);
+//             res.status(200).json(result);
+//         }
+//     }
+//     catch(err){
+//         res.status(400).json({errMessage: err});
+//     }
+// };
 
-exports.deleteTodo = async (req, res) => {
-    try{
-        const todoId = req.params.id;
-        const result = await todo.deleteTodo(todoId); //userId
-        res.status(200).json({success : result});
-    }
-    catch(err){
-        res.status(400).json({errMessage: err});
-    }
-};
+// exports.deleteTodo = async (req, res) => {
+//     try{
+//         const todoId = req.params.id;
+//         const result = await todo.deleteTodo(todoId); //userId
+//         res.status(200).json({success : result});
+//     }
+//     catch(err){
+//         res.status(400).json({errMessage: err});
+//     }
+// };
--- models/todo.js
+++ models/todo.js
@@ -1,13 +1,20 @@
-const Todo = require("../schemas/todo");
-const moment = require("moment");
-
-exports.createTodo = async (work, isDone, color) => {
-  const today = moment().startOf("day");
+// user가 있는 버전. 이 repo에선 user가 없는 버전으로 동작함.
+exports.createTodo = async (work, isDone, color, user) => {
+  let today = moment();
+  const todayStart = moment(today).startOf("day");
+  const todayEnd = moment(today).endOf("day");
   const existedTodo = await Todo.findOne({
-    createdAt: {
-      $gte: today.toDate(),
-      $lte: moment(today).endOf("day").toDate(),
-    },
+    $and: [
+      {
+        createdAt: {
+          $gte: todayStart.toDate(),
+          $lte: todayEnd.toDate(),
+        },
+      },
+      {
+        userId: user._id,
+      },
+    ],
   });
 
   if (existedTodo) {
@@ -15,18 +22,29 @@ exports.createTodo = async (work, isDone, color) => {
     const result = await existedTodo.save();
     return result.todoArr[result.todoArr.length - 1];
   } else {
-    const result = await Todo.create({ todoArr: [{ work, isDone, color }] });
+    const result = await Todo.create({
+      userId: user._id,
+      todoArr: [{ work, isDone, color }],
+    });
     return result.todoArr[result.todoArr.length - 1];
   }
 };
 
-exports.getTodo = async (dayData) => {
-  const day = moment(dayData).startOf("day");
+exports.getTodo = async (dayData, user) => {
+  const dayStart = moment(dayData).startOf("day");
+  const dayEnd = moment(dayStart).endOf("day");
   const existedTodo = await Todo.findOne({
-    createdAt: {
-      $gte: day.toDate(),
-      $lte: moment(day).endOf("day").toDate(),
-    },
+    $and: [
+      {
+        createdAt: {
+          $gte: dayStart.toDate(),
+          $lte: dayEnd.toDate(),
+        },
+      },
+      {
+        userId: user._id,
+      },
+    ],
   });
   if (existedTodo) {
     return existedTodo;
@@ -35,72 +53,223 @@ exports.getTodo = async (dayData) => {
   }
 };
 
-exports.putTodo = async (todoId, work, color) => {
-  const today = moment().startOf("day");
+exports.putTodo = async (todoId, work, color, user) => {
+  let today = moment();
+
+  const todayStart = moment(today).startOf("day");
+  const todayEnd = moment(today).endOf("day");
   const existedTodo = await Todo.findOne({
-    createdAt: {
-      $gte: today.toDate(),
-      $lte: moment(today).endOf("day").toDate(),
-    },
+    $and: [
+      {
+        createdAt: {
+          $gte: todayStart.toDate(),
+          $lte: todayEnd.toDate(),
+        },
+      },
+      {
+        userId: user._id,
+      },
+    ],
   });
-  let todoArrIdx = undefined;
+  let todoArrIdx = false;
   if (existedTodo) {
     existedTodo.todoArr.map((todo, idx) => {
       if (todo._id.equals(todoId)) {
-        todoArrIdx = idx;
+        console.log(todoId);
+        todoArrIdx = idx.toString();
+        existedTodo.todoArr[idx].work = work;
+        existedTodo.todoArr[idx].color = color;
       }
     });
-    existedTodo.todoArr[todoArrIdx].work = work;
-    existedTodo.todoArr[todoArrIdx].color = color;
+    if (!todoArrIdx) {
+      throw new Error("todo id가 없거나 일치하지 않습니다.");
+    }
     const result = await existedTodo.save();
-    return result.todoArr[todoArrIdx];
+    return result.todoArr[Number(todoArrIdx)];
   } else {
-    throw new Error("데이터가 없습니다.");
+    throw new Error("todo에 데이터가 없습니다.");
   }
 };
 
-exports.isDoneTodo = async (todoId, isDone) => {
-  const today = moment().startOf("day");
+exports.isDoneTodo = async (todoId, isDone, user) => {
+  let today = moment();
+  const todayStart = moment(today).startOf("day");
+  const todayEnd = moment(today).endOf("day");
   const existedTodo = await Todo.findOne({
-    createdAt: {
-      $gte: today.toDate(),
-      $lte: moment(today).endOf("day").toDate(),
-    },
+    $and: [
+      {
+        createdAt: {
+          $gte: todayStart.toDate(),
+          $lte: todayEnd.toDate(),
+        },
+      },
+      {
+        userId: user._id,
+      },
+    ],
   });
-  let todoArrIdx = undefined;
+  let todoArrIdx = false;
   if (existedTodo) {
     existedTodo.todoArr.map((todo, idx) => {
       if (todo._id.equals(todoId)) {
-        todoArrIdx = idx;
+        todoArrIdx = idx.toString();
+        existedTodo.todoArr[idx].isDone = isDone;
       }
     });
-    existedTodo.todoArr[todoArrIdx].isDone = isDone;
+    if (!todoArrIdx) {
+      throw new Error("todo id가 없거나 일치하지 않습니다.");
+    }
     const result = await existedTodo.save();
-    return result.todoArr[todoArrIdx];
+    return result.todoArr[Number(todoArrIdx)];
   } else {
-    throw new Error("데이터가 없습니다.");
+    throw new Error("todo에 데이터가 없습니다.");
   }
 };
 
-exports.deleteTodo = async (todoId) => {
-  const today = moment().startOf("day");
+exports.deleteTodo = async (todoId, user) => {
+  let today = moment();
+  const todayStart = moment(today).startOf("day");
+  const todayEnd = moment(today).endOf("day");
   const existedTodo = await Todo.findOne({
-    createdAt: {
-      $gte: today.toDate(),
-      $lte: moment(today).endOf("day").toDate(),
-    },
+    $and: [
+      {
+        createdAt: {
+          $gte: todayStart.toDate(),
+          $lte: todayEnd.toDate(),
+        },
+      },
+      {
+        userId: user._id,
+      },
+    ],
   });
-  let todoArrIdx = undefined;
+  let todoArrIdx = false;
   if (existedTodo) {
     existedTodo.todoArr.map((todo, idx) => {
       if (todo._id.equals(todoId)) {
-        todoArrIdx = idx;
+        todoArrIdx = idx.toString();
+        existedTodo.todoArr.splice(idx, 1);
       }
     });
-    existedTodo.todoArr.splice(todoArrIdx, 1);
+    if (!todoArrIdx) {
+      throw new Error("todo id가 없거나 일치하지 않습니다.");
+    }
     await existedTodo.save();
     return true;
   } else {
-    throw new Error("데이터가 없습니다.");
+    throw new Error("todo에 데이터가 없습니다.");
   }
 };
+
+
+
+// user가 없는 버전.
+// const Todo = require("../schemas/todo");
+// const moment = require("moment");
+
+// exports.createTodo = async (work, isDone, color) => {
+//   const today = moment().startOf("day");
+//   const existedTodo = await Todo.findOne({
+//     createdAt: {
+//       $gte: today.toDate(),
+//       $lte: moment(today).endOf("day").toDate(),
+//     },
+//     // function (err, result) {
+//     //   if(err) return new Error(err);
+//     // }
+//   });
+
+//   if (existedTodo) {
+//     existedTodo.todoArr.push({ work, isDone, color });
+//     const result = await existedTodo.save();
+//     return result.todoArr[result.todoArr.length - 1];
+//   } else {
+//     const result = await Todo.create({ todoArr: [{ work, isDone, color }] });
+//     return result.todoArr[result.todoArr.length - 1];
+//   }
+// };
+
+// exports.getTodo = async (dayData) => {
+//   const day = moment(dayData).startOf("day");
+//   const existedTodo = await Todo.findOne({
+//     createdAt: {
+//       $gte: day.toDate(),
+//       $lte: moment(day).endOf("day").toDate(),
+//     },
+//   });
+//   if (existedTodo) {
+//     return existedTodo;
+//   } else {
+//     return `${dayData} 에 저장된 할일 리스트가 없습니다.`;
+//   }
+// };
+
+// exports.putTodo = async (todoId, work, color) => {
+//   const today = moment().startOf("day");
+//   const existedTodo = await Todo.findOne({
+//     createdAt: {
+//       $gte: today.toDate(),
+//       $lte: moment(today).endOf("day").toDate(),
+//     },
+//   });
+//   let todoArrIdx = undefined;
+//   if (existedTodo) {
+//     existedTodo.todoArr.map((todo, idx) => {
+//       if (todo._id.equals(todoId)) {
+//         todoArrIdx = idx;
+//       }
+//     });
+//     existedTodo.todoArr[todoArrIdx].work = work;
+//     existedTodo.todoArr[todoArrIdx].color = color;
+//     const result = await existedTodo.save();
+//     return result.todoArr[todoArrIdx];
+//   } else {
+//     throw new Error("데이터가 없습니다.");
+//   }
+// };
+
+// exports.isDoneTodo = async (todoId, isDone) => {
+//   const today = moment().startOf("day");
+//   const existedTodo = await Todo.findOne({
+//     createdAt: {
+//       $gte: today.toDate(),
+//       $lte: moment(today).endOf("day").toDate(),
+//     },
+//   });
+//   let todoArrIdx = undefined;
+//   if (existedTodo) {
+//     existedTodo.todoArr.map((todo, idx) => {
+//       if (todo._id.equals(todoId)) {
+//         todoArrIdx = idx;
+//       }
+//     });
+//     existedTodo.todoArr[todoArrIdx].isDone = isDone;
+//     const result = await existedTodo.save();
+//     return result.todoArr[todoArrIdx];
+//   } else {
+//     throw new Error("데이터가 없습니다.");
+//   }
+// };
+
+// exports.deleteTodo = async (todoId) => {
+//   const today = moment().startOf("day");
+//   const existedTodo = await Todo.findOne({
+//     createdAt: {
+//       $gte: today.toDate(),
+//       $lte: moment(today).endOf("day").toDate(),
+//     },
+//   });
+//   let todoArrIdx = undefined;
+//   if (existedTodo) {
+//     existedTodo.todoArr.map((todo, idx) => {
+//       if (todo._id.equals(todoId)) {
+//         todoArrIdx = idx;
+//       }
+//     });
+//     existedTodo.todoArr.splice(todoArrIdx, 1);
+//     await existedTodo.save();
+//     return true;
+//   } else {
+//     throw new Error("데이터가 없습니다.");
+//   }
+// };
